8e52ea7d842cbfcbaee9b44c39ef302ccb3dc6d4
leviettien1808 <leviettien1808@gmail.com>
2025-08-21 23:19:17 +0700

    Unique index

--- indexes_database/03-create-index.sql
+++ indexes_database/03-create-index.sql
@@ -7,3 +7,6 @@ CREATE TABLE <table_name> (
     ... column definitions,
     INDEX <index_name> (<column>)
 );
+
+-- Tạo unique index hoặc khi tạo bảng
+CREATE UNIQUE INDEX emailidx ON users (email);
